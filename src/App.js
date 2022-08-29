e1fc1174d427454407f43747a203d5a9f82ba015
Jake Walsh <jkwalsh127@gmail.com>
2022-08-29 11:28:40 -0700

    added amplify auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+// import MainContent from "./components/MainContent/MainContent";
+// import SupportingContent from "./components/SupportingConent/SupportingContent";
+// import Header from "./components/Header/Header";
+import { Authenticator } from '@aws-amplify/ui-react';
+import "@aws-amplify/ui-react/styles.css";
+// import { DataStore } from '@aws-amplify/datastore';
+// import { User } from './models';
 
-function App() {
+
+export default function App() {
+  // const [user, setUser] = useState([]);
+
+  // useEffect(() => {
+  //   fetchUser();
+  // }, []);
+
+  // async function fetchUser() {
+  //   const userModels = await DataStore.query(User);
+  //   console.log(userModels);
+  //   setUser(userModels);
+  // }
+
+  // const [supporting, setSupporting] = useState("Overview");
+  // const handleSupportingChange = (selection) => setSupporting(selection);
+
+  // const [main, setMain] = useState("Overview");
+  // const handleMainChange = (selection) => setMain(selection);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut }) => (
+        <div className="app-container">
+            {/* <Header  signOut={signOut} handleSupportingChange={handleSupportingChange} handleMainChange={handleMainChange} />
+            <SupportingContent supporting={supporting} user={user} />
+            <MainContent main={main} /> */}
+        </div>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
